3a871ad35ceba29d0069a2246119e2b8a333ab4d
JiaLong0209 <smt02091395@gmail.com>
2024-09-24 20:35:18 +0800

    240924 Add start i3/awesome script for arch

---
+++ proot-distro/arch/arch_login_isolated.sh
@@ -0,0 +1,2 @@
+proot-distro login  --isolated --fix-low-ports archlinux --user user --shared-tmp
+# proot-distro login  --termux-home --fix-low-ports --termux-home --shared-tmp archlinux
---
+++ proot-distro/arch/startawesome_arch.sh
@@ -0,0 +1,23 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+USER=$1
+
+if [[ -z $USER ]]; then
+  USER="user"
+fi
+
+# Kill open X11 processes
+kill -9 $(pgrep -f "termux.x11") 2>/dev/null
+
+virgl_test_server_android &
+#
+# Enable PulseAudio over Network
+pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
+
+# Prepare termux-x11 session
+export XDG_RUNTIME_DIR=${TMPDIR}
+termux-x11 :0 >/dev/null &
+
+proot-distro login archlinux --user $USER --shared-tmp  -- /bin/bash -c  'export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=${TMPDIR} && su -l $USER -c "env DISPLAY=:0 awesome"'
+
+exit 0
